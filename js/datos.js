347bd180cc6f71be73eb2028ca1ea8007d248eb0
Mxii <max.veas@duocuc.cl>
2026-09-05 14:48:25 -0400

    aca se crea la base de datos de componentes de hardware

--- js/datos.js
+++ js/datos.js
@@ -1,25 +1,18 @@
-const buildsDestacadas = [
-    {
-        nombre: "PC Gamer Básico",
-        procesador: "AMD Ryzen 5",
-        grafica: "RTX 4060",
-        ram: "16 GB",
-        precio: 850000
-    },
-
-    {
-        nombre: "PC Gamer Avanzado",
-        procesador: "Intel Core i7",
-        grafica: "RTX 4070",
-        ram: "32 GB",
-        precio: 1400000
-    },
-
-    {
-        nombre: "PC Gamer Pro",
-        procesador: "AMD Ryzen 7",
-        grafica: "RTX 4080",
-        ram: "32 GB",
-        precio: 1900000
-    }
-];
+const productosDB = {
+    cpus: [
+        { id: "cpu-1", nombre: "AMD Ryzen 5 7600X", socket: "AM5", tdp: 105, score: 75, precio: 220000 },
+        { id: "cpu-2", nombre: "Intel Core i7-13700K", socket: "LGA1700", tdp: 125, score: 88, precio: 380000 }
+    ],
+    motherboards: [
+        { id: "mobo-1", nombre: "ASUS TUF B650-PLUS", socket: "AM5", precio: 180000 },
+        { id: "mobo-2", nombre: "MSI MAG Z790 TOMAHAWK", socket: "LGA1700", precio: 260000 }
+    ],
+    gpus: [
+        { id: "gpu-1", nombre: "NVIDIA RTX 4060 8GB", tdp: 115, score: 70, precio: 340000 },
+        { id: "gpu-2", nombre: "NVIDIA RTX 4070 Super 12GB", tdp: 220, score: 88, precio: 650000 }
+    ],
+    psus: [
+        { id: "psu-1", nombre: "Corsair CV650 650W 80+", watts: 650, precio: 65000 },
+        { id: "psu-2", nombre: "EVGA SuperNOVA 750W Gold", watts: 750, precio: 95000 }
+    ]
+};
