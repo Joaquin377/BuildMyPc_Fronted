89953cf2aa6b8a0f7c771929cf21b4322e9393b5
Mxii <max.veas@duocuc.cl>
2026-09-05 15:05:38 -0400

    se corrigio estructura

--- js/datos.js
+++ js/datos.js
@@ -1,7 +1,8 @@
 const productosDB = {
     cpus: [
         { id: "cpu-1", nombre: "AMD Ryzen 5 7600X", socket: "AM5", tdp: 105, score: 75, precio: 220000 },
-        { id: "cpu-2", nombre: "Intel Core i7-13700K", socket: "LGA1700", tdp: 125, score: 88, precio: 380000 }
+        { id: "cpu-2", nombre: "Intel Core i7-13700K", socket: "LGA1700", tdp: 125, score: 88, precio: 380000 },
+        { id: "cpu-3", nombre: "AMD Ryzen 7 7800X3D", socket: "AM5", tdp: 120, score: 98, precio: 420000 }
     ],
     motherboards: [
         { id: "mobo-1", nombre: "ASUS TUF B650-PLUS", socket: "AM5", precio: 180000 },
@@ -9,10 +10,10 @@ const productosDB = {
     ],
     gpus: [
         { id: "gpu-1", nombre: "NVIDIA RTX 4060 8GB", tdp: 115, score: 70, precio: 340000 },
-        { id: "gpu-2", nombre: "NVIDIA RTX 4070 Super 12GB", tdp: 220, score: 88, precio: 650000 }
+        { id: "gpu-2", nombre: "NVIDIA RTX 4070 Super", tdp: 220, score: 88, precio: 650000 }
     ],
     psus: [
-        { id: "psu-1", nombre: "Corsair CV650 650W 80+", watts: 650, precio: 65000 },
-        { id: "psu-2", nombre: "EVGA SuperNOVA 750W Gold", watts: 750, precio: 95000 }
+        { id: "psu-1", nombre: "Corsair CV650 650W", watts: 650, precio: 65000 },
+        { id: "psu-2", nombre: "EVGA SuperNOVA 850W", watts: 850, precio: 115000 }
     ]
 };
